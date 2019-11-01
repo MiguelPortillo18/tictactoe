64048dab24ad70dce6eef5aa4b7c2969e67c9700
MiguelPortillo18 <00043818@uca.edu.sv>
2019-11-01 11:34:18 -0600

    Updates

---
+++ precache-manifest.383690941a57d0b184ccad43a1cca86d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484a118d3052cb2bfbafb8758b8a8c29",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "9c0afc1acb22d3e2864a",
+    "url": "/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "9c0afc1acb22d3e2864a",
+    "url": "/tictactoe/static/js/main.c676d35b.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
